fd69bbf8859f69473a77a9d91b5e4005135feb67
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2022-06-01 11:04:22 +0700

    Setting project and create abstrack landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @next/next/no-sync-scripts */
+import '../styles/landingpage.css';
+import '../styles/instructor.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/instructor.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StarSharp } from 'react-ionicons';
+
+export default function Instructor() {
+  return (
+    <main>
+      <div className="container">
+        <div className="profile">
+          <div className="profile-image">
+            <img
+              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880"
+              alt="profile-image"
+            />
+          </div>
+          <div className="profile-info">
+            <h1>John Doe</h1>
+            <p>Farmer</p>
+          </div>
+        </div>
+        <div className="operations">
+          <div className="operations__item">
+            <a href="#" className="">
+              My Course
+            </a>
+          </div>
+          <div className="operations__item operations__item--active">
+            <a href="#" className="">
+              Review
+            </a>
+          </div>
+          <div className="operations__item">
+            <a href="#" className="">
+              Testimonial
+            </a>
+          </div>
+        </div>
+
+        <div className="about">
+          <div className="about-content">
+            <p className="about-content-paragraph">
+              For me, teaching is like learning something new that I am
+              passionate about which is cooking a delicious food for people. It
+              can also make the good relationship between member of my lovely
+              family.
+            </p>
+            <p className="about-student">28,309 students</p>
+            <div className="about-content-star">
+              <StarSharp color={'#F7C531'} height="22px" width="22px" />
+              <StarSharp color={'#F7C531'} height="22px" width="22px" />
+              <StarSharp color={'#F7C531'} height="22px" width="22px" />
+              <StarSharp color={'#F7C531'} height="22px" width="22px" />
+              <StarSharp color={'#F7C531'} height="22px" width="22px" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
