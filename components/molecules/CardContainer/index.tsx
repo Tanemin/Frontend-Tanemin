0a11c86bebde88ee8b236092934f12492547b7f5
anwark7 <anwarkhairuddin11@gmail.com>
2022-06-17 15:31:52 +0700

    refactor: card container

--- components/molecules/CardContainer/index.tsx
+++ components/molecules/CardContainer/index.tsx
@@ -1,38 +1,36 @@
-import { Image } from '@chakra-ui/react';
-import Link from 'next/link';
-import React from 'react';
+import { Image } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
 interface CardData {
-  imageUrl?: string;
-  plantName: string;
-  author: string;
-  href: string;
+    imageUrl?: string;
+    plantName: string;
+    author: string;
+    href: string;
 }
 
 export default function CardContainer(props: CardData) {
-  const {
-    imageUrl = '/assets/content-img.jpg',
-    plantName,
-    author,
-    href,
-  } = props;
-  return (
-    <>
-      <div className="card-content">
-        <div className="card-content__image">
-          <Image
-            src={imageUrl}
-            fallbackSrc="https://via.placeholder.com/150"
-            alt="Plants Category"
-          />
-        </div>
-        <div className="card-content__info">
-          <Link href={`/detail/${href}`}>
-            <a className="food-name">{plantName}</a>
-          </Link>
-          <p className="food-author">{author}</p>
-          <div className="star-container"></div>
-        </div>
-      </div>
-    </>
-  );
+    const {
+        imageUrl = "/assets/content-img.jpg",
+        plantName,
+        author,
+        href,
+    } = props;
+    return (
+        <Link href={`/detail/${href}`} passHref>
+            <div className="card-content hover-pointer">
+                <div className="card-content__image">
+                    <Image
+                        src={imageUrl}
+                        fallbackSrc="https://via.placeholder.com/150"
+                        alt="Plants Category"
+                    />
+                </div>
+                <div className="card-content__info">
+                    <p className="food-name">{plantName}</p>
+                    <p className="food-author">{author}</p>
+                    <div className="star-container"></div>
+                </div>
+            </div>
+        </Link>
+    );
 }
