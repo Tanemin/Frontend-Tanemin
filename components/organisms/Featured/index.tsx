43072cabf7cbf3f06535844acb0a481b20a5f4f8
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2022-06-19 11:20:37 +0700

    refactor navbar element and edit.tsx user page

--- components/organisms/Featured/index.tsx
+++ components/organisms/Featured/index.tsx
@@ -1,43 +1,43 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { PlantTypes } from "../../../services/data-types";
-import { getTopPlant } from "../../../services/plant-list";
-import CardContainer from "../../molecules/CardContainer";
+import React, { useCallback, useEffect, useState } from 'react';
+import { PlantTypes } from '../../../services/data-types';
+import { getTopPlant } from '../../../services/plant-list';
+import CardContainer from '../../molecules/CardContainer';
 
 export default function Featured() {
-    const [topPlant, setTopPlant] = useState([]);
+  const [topPlant, setTopPlant] = useState([]);
 
-    const topPlantAPI = useCallback(async () => {
-        try {
-            const data = await getTopPlant();
-            setTopPlant(data);
-        } catch (error) {
-            console.log(error);
-        }
-    }, []);
+  const topPlantAPI = useCallback(async () => {
+    try {
+      const data = await getTopPlant();
+      setTopPlant(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-    useEffect(() => {
-        topPlantAPI();
-    }, [topPlantAPI]);
+  useEffect(() => {
+    topPlantAPI();
+  }, [topPlantAPI]);
 
-    return (
-        <>
-            <section className="featured">
-                <h2 className="featured_title">Tanaman Terbaik Kami</h2>
+  return (
+    <>
+      <section className="featured">
+        <h2 className="featured_title">Paling banyak dicari</h2>
 
-                <div className="item_list">
-                    {topPlant.map((item: PlantTypes) => {
-                        return (
-                            <CardContainer
-                                key={item.id}
-                                plantName={item.plantName}
-                                author="SehatPedia"
-                                imageUrl={item.imageCover}
-                                href={`/detail/${item.id}`}
-                            />
-                        );
-                    })}
-                </div>
-            </section>
-        </>
-    );
+        <div className="item_list">
+          {topPlant.map((item: PlantTypes) => {
+            return (
+              <CardContainer
+                key={item.id}
+                plantName={item.plantName}
+                author="SehatPedia"
+                imageUrl={item.imageCover}
+                href={`/detail/${item.id}`}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
---
+++ components/organisms/Navbar/item.ts
@@ -0,0 +1,21 @@
+export interface NavItem {
+    label: string;
+    subLabel?: string;
+    children?: Array<NavItem>;
+    href?: string;
+}
+
+export const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: 'Home',
+        href: '/',
+    },
+    {
+        label: 'Explore',
+        href: '/explore',
+    },
+    {
+        label: 'Contact Us',
+        href: '/contact',
+    },
+];
